76304487619edeeb231e67c03ed6624a144e9504
Adrian Rogers <adritek@gmail.com>
2015-02-14 11:14:04 +1000

    Added a click listener to start the quiz, some styling for the button

--- fun quiz/js/ascripts.js
+++ fun quiz/js/ascripts.js
@@ -1,18 +1,64 @@
-var score = 0; //initialise score
-var quiz = [
-  ["What is Superman's real name?", "Clarke Kent"],
-  ["What is Wonder Woman's real name?", "Dianne Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"]
-];
+// Initialise variables
+var $start = document.getElementById("start");
 
-for ( var i = 0, max = quiz.length; i < max; i++ ){
-  var answer = prompt( quiz[i][0] );
+var quiz = {
+  "name":"Super Hero Name Quiz",
+  "description":"How many super heroes can you name?",
+  "question":"What is the real name of ",
+  "questions": [
+    { "question": "Superman", "answer": "Clarke Kent" },
+    { "question": "Batman", "answer": "Bruce Wayne" },
+    { "question": "Wonder Woman", "answer": "Dianna Prince" }
+  ]
+}
 
-  if (answer.toLowerCase() === quiz[i][1].toLowerCase()){
-    score++;
-  } else {
-    alert("Incorrect");
+// Dom references
+var $question = document.getElementById("question");
+var $score = document.getElementById("score");
+var $feedback = document.getElementById("feedback");
+
+// Init
+$start.addEventListener("click", function(){
+  play(quiz);
+}, false);
+
+// Function definitions
+function update(element, content, klass){
+  var p = element.firstChild || document.createElement('h3');
+  p.textContent = content;
+  element.appendChild(p);
+  if(klass){
+    p.className = klass;
   }
 }
 
-alert("Game Over, you scored " + score + " points");
+function play(quiz){
+  var score = 0;
+  update($score, score);
+  for (var i= 0, question, answer, max = quiz.questions.length; i < max; i++){
+    question = quiz.questions[i].question;
+    answer = ask(question);
+    check(answer);
+  }
+
+  function ask(question) {
+    update($question,quiz.question + question);
+    return prompt("Enter your answer:");
+  }
+
+  function check(answer){
+    if (answer.toLowerCase() === quiz.questions[i].answer.toLowerCase()){
+      update($feedback, "Correct!", "right");
+      score++;
+      update($score, score)
+    } else {
+      update($feedback, "Wrong", "wrong");
+    }
+  }
+
+  // end game
+  gameOver();
+  function gameOver(){
+    alert("You scored: " + score + " out of " + quiz.length + "!");
+  }
+}
--- fun quiz/js/scripts.js
+++
@@ -1,3 +0,0 @@
-var question = "What is Superman's real name?"
-var answer = prompt(question);
-alert("You answered " + answer);

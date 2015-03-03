9eaffce238321e458875076c26474e861babf336
Adrian Rogers <adritek@gmail.com>
2015-03-03 22:16:43 +1000

    Big refactor and included form processing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # jsquiz
 A simple quiz game using objects, and vanilla JavaScript.
-This line is just a test to see if my alias worked
+This line is just a test to see if my alias worked - it worked
+git --set-upstream origin master (now i just have to git push)
--- fun quiz/js/ascripts.js
+++ fun quiz/js/ascripts.js
@@ -1,64 +1,96 @@
-// Initialise variables
-var $start = document.getElementById("start");
-
 var quiz = {
-  "name":"Super Hero Name Quiz",
-  "description":"How many super heroes can you name?",
-  "question":"What is the real name of ",
-  "questions": [
-    { "question": "Superman", "answer": "Clarke Kent" },
-    { "question": "Batman", "answer": "Bruce Wayne" },
-    { "question": "Wonder Woman", "answer": "Dianna Prince" }
-  ]
+"name":"Super Hero Name Quiz",
+"description":"How many super heroes can you name?",
+"question":"What is the real name of ",
+"questions": [
+{ "question": "Superman", "answer": "Clarke Kent" },
+{ "question": "Batman", "answer": "Bruce Wayne" },
+{ "question": "Wonder Woman", "answer": "Dianna Prince" }
+]
 }
 
-// Dom references
+//// views ////
 var $question = document.getElementById("question");
 var $score = document.getElementById("score");
 var $feedback = document.getElementById("feedback");
+var $start = document.getElementById("start");
+var $form = document.getElementById("answer");
 
-// Init
-$start.addEventListener("click", function(){
-  play(quiz);
-}, false);
+/// view functions ///
 
-// Function definitions
-function update(element, content, klass){
-  var p = element.firstChild || document.createElement('h3');
+function update(element,content,klass) {
+  var p = element.firstChild || document.createElement("p");
   p.textContent = content;
   element.appendChild(p);
-  if(klass){
+  if(klass) {
     p.className = klass;
   }
 }
 
+function hide(element) {
+  element.style.display = "none";
+}
+
+function show(element) {
+  element.style.display = "block";
+}
+
+// Event listeners
+$start.addEventListener('click', function() { play(quiz) } , false);
+
+// hide the form at the start of the game
+hide($form);
+
+//// function definitions ////
+
 function play(quiz){
-  var score = 0;
-  update($score, score);
-  for (var i= 0, question, answer, max = quiz.questions.length; i < max; i++){
-    question = quiz.questions[i].question;
-    answer = ask(question);
-    check(answer);
+  var score = 0 // initialize score
+  update($score,score);
+  // hide button and show form
+  hide($start);
+  show($form);
+  // add event listener to form for when it's submitted
+  $form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    check($form[0].value);
+  }, false);
+  var i = 0;
+  chooseQuestion();
+
+  // nested functions
+
+  function chooseQuestion() {
+    var question = quiz.questions[i].question;
+    ask(question);
   }
 
   function ask(question) {
     update($question,quiz.question + question);
-    return prompt("Enter your answer:");
+    $form[0].value = "";
+    $form[0].focus();
   }
 
-  function check(answer){
-    if (answer.toLowerCase() === quiz.questions[i].answer.toLowerCase()){
-      update($feedback, "Correct!", "right");
+  function check(answer) {
+    if(answer.toLowerCase() === quiz.questions[i].answer.toLowerCase()){
+      update($feedback,"Correct!","correct");
+      // increase score by 1
       score++;
-      update($score, score)
+      update($score,score)
     } else {
-      update($feedback, "Wrong", "wrong");
+      update($feedback,"Wrong!","wrong");
+    }
+    i++;
+    if(i === quiz.questions.length) {
+      gameOver();
+    } else {
+      chooseQuestion();
     }
   }
 
-  // end game
-  gameOver();
   function gameOver(){
-    alert("You scored: " + score + " out of " + quiz.length + "!");
+    // inform the player that the game has finished and tell them how many points they have scored
+    update($question,"Game Over, you scored " + score + " points");
+    hide($form);
+    show($start);
   }
 }
